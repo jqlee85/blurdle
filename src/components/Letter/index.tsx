e7c14f5b6e661adf88389308397e909934bdb8dc
Jesse Lee <jesselee@Jesses-MacBook-Air.local>
2022-09-20 22:06:36 -0700

    Add basic keyboard.

--- src/components/Letter/index.tsx
+++ src/components/Letter/index.tsx
@@ -1,5 +1,13 @@
 import './styles.css';
 
-export default () => {
-    return <></>;
+export interface ILetterProps {
+    onClickHandler?: (e:React.FormEvent<HTMLInputElement>) => void;
+    evaluatedState?: string;
+    letter: string;
+}
+
+export default ({letter = '', onClickHandler = () => {}, evaluatedState = "unevaluated"}) => {
+    return <div className={`blurdle-letter evaluated-state_${evaluatedState}`}>
+        <button onClick={onClickHandler}>{letter}</button>
+    </div>;
 }
--- src/components/LettersContainer/index.tsx
+++ src/components/LettersContainer/index.tsx
@@ -1,5 +1,18 @@
 import './styles.css';
+import Letter from '../Letter';
+
+const keyboard = [
+    ['Q','W','E','R','T','Y','U','I','O','P'],
+    ['A','S','D','F','G','H','J','K','L',],
+    ['Z','X','C','V','B','N','M',],
+];
 
 export default () => {
-    return <></>;
+    return (<form id="blurdle-keyboard">
+        {keyboard.map((keyboardRow,i)=>{
+            return (<div className="blurdle-keyboard-row">
+                { keyboardRow.map((letter,i)=><Letter letter={letter} key={`letter-${i}`}/> )}
+            </div>);
+        })}
+    </form>);
 }
