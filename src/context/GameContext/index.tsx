48f0229af9b9387abb67ba932c3a1e8d6b05dbef
Jesse Lee <jesselee@Jesses-MacBook-Air.local>
2022-09-22 15:14:04 -0700

    Add immer package and add basic space updating/backspace to reducer.

--- src/context/GameContext/index.tsx
+++ src/context/GameContext/index.tsx
@@ -1,7 +1,9 @@
 import React, {useReducer, useState, Dispatch} from "react";
 import gameReducer from './reducer';
+import { useImmerReducer } from 'use-immer';
 import createInitialGameState from './create-initial-game-state';
 import {GameData, GameState, IGameAction} from '../../types';
+
 
 export const GameContext = React.createContext<{state: GameState, dispatch: Dispatch<IGameAction>}>({
     state: {} as GameState,
@@ -19,7 +21,8 @@ const GameContextProvider = ({
 }:IGameContextProviderProps) => {
 
     // Overall game state
-    const [state, dispatch] = useReducer(gameReducer, createInitialGameState(gameData));
+    const [state, dispatch] = useImmerReducer(gameReducer, createInitialGameState(gameData));
+    console.log({state})
 
     return <GameContext.Provider
         value={{state, dispatch}}
