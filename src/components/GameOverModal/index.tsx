abb71efdf798b4801749043ed80975f5aa1379d1
Jesse Lee <jesselee@Jesses-MacBook-Air.local>
2022-09-22 20:34:17 -0700

    Minor styling.

--- src/components/GameOverModal/index.tsx
+++ src/components/GameOverModal/index.tsx
@@ -18,14 +18,14 @@ export default ({
             className="blurdle-game-over-modal"
         >
             {gameStatus === GAME_STATUS.WON &&
-                <div className="blurdle-modal-content">
+                <div className="blurdle-modal-content won">
                     <h2>{`Congrats!`}</h2>
                     <p>{`You guessed in ${numGuesses} ${numGuesses > 1 ? `guesses`: `guess`}`}</p>
                     <p>{`Word was `}<span className="solution">{gameSolution.solution}</span></p>
                 </div>
             }
             {gameStatus === GAME_STATUS.LOST &&
-                <div className="blurdle-modal-content">
+                <div className="blurdle-modal-content lost">
                     <h2>{`Bummer.`}</h2>
                     <p>{`Word was `}<span className="solution">{gameSolution.solution}</span></p>
                     <p>{`Better luck next time.`}</p>
