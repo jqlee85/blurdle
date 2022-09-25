0574bc883ead4e727cfda5c5e208c0b21ff3f4ad
Jesse Lee <jesselee@Jesses-MacBook-Air.local>
2022-09-24 23:58:23 -0700

    Use flexbox for keyboard.

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -7,8 +7,7 @@ import { KeyboardRow } from '../../types';
 const keyboard: KeyboardRow[] = [
     [{type:'letter', size: 'sm', value:'Q'},{type:'letter', size: 'sm', value:'W'},{type:'letter', size: 'sm', value:'E'},{type:'letter', size: 'sm', value:'R'},{type:'letter', size: 'sm', value:'T'},{type:'letter', size: 'sm', value:'Y'},{type:'letter', size: 'sm', value:'U'},{type:'letter', size: 'sm', value:'I'},{type:'letter', size: 'sm', value:'O'},{type:'letter', size: 'sm', value:'P'}],
     [{type:'letter', size: 'sm', value:'A'},{type:'letter', size: 'sm', value:'S'},{type:'letter', size: 'sm', value:'D'},{type:'letter', size: 'sm', value:'F'},{type:'letter', size: 'sm', value:'G'},{type:'letter', size: 'sm', value:'H'},{type:'letter', size: 'sm', value:'J'},{type:'letter', size: 'sm', value:'K'},{type:'letter', size: 'sm', value:'L'},],
-    [{type:'letter', size: 'sm', value:'Z'},{type:'letter', size: 'sm', value:'X'},{type:'letter', size: 'sm', value:'C'},{type:'letter', size: 'sm', value:'V'},{type:'letter', size: 'sm', value:'B'},{type:'letter', size: 'sm', value:'N'},{type:'letter', size: 'sm', value:'M'}],
-    [{type:'submit', size: 'lg', value:'Enter'},{type:'backspace',  size: 'lg', value:'Delete'}]
+    [{type:'submit', size: 'lg', value:'Enter'},{type:'letter', size: 'sm', value:'Z'},{type:'letter', size: 'sm', value:'X'},{type:'letter', size: 'sm', value:'C'},{type:'letter', size: 'sm', value:'V'},{type:'letter', size: 'sm', value:'B'},{type:'letter', size: 'sm', value:'N'},{type:'letter', size: 'sm', value:'M'},{type:'backspace',  size: 'lg', value:'Del'}]
 ];
 
 export interface IKeyboardProps {
@@ -20,7 +19,7 @@ const Keyboard = ({
         id="blurdle-keyboard"
     >
         {keyboard.map((keyboardRow,j)=>{
-            return (<div className="blurdle-keyboard-row" key={`keyboard-row_${j}`}>
+            return (<div className={`blurdle-keyboard-row blurdle-keyboard-row_${j}`} key={`keyboard-row_${j}`}>
                 { keyboardRow.map((buttonData,i)=>{
                     return <Letter
                         buttonData={buttonData}
